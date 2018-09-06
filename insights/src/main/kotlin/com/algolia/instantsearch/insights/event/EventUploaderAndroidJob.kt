c30b9541d0f06a5dab5c64ab95cbe1f984322758
Quentin LITZLER <qlitzler@gmail.com>
2018-09-06 14:56:13 +0200

    [Insights] Use Delegate in AndroidJob

--- insights/src/main/kotlin/com/algolia/instantsearch/insights/event/EventUploaderAndroidJob.kt
+++ insights/src/main/kotlin/com/algolia/instantsearch/insights/event/EventUploaderAndroidJob.kt
@@ -1,6 +1,8 @@
 package com.algolia.instantsearch.insights.event
 
 import android.content.Context
+import android.content.SharedPreferences
+import com.algolia.instantsearch.insights.database.SharedPreferencesDelegate
 import com.evernote.android.job.JobManager
 import com.evernote.android.job.JobRequest
 import java.util.concurrent.TimeUnit
@@ -9,10 +11,7 @@ import java.util.concurrent.TimeUnit
 internal class EventUploaderAndroidJob(context: Context) : EventUploader {
 
     private val preferences = context.getSharedPreferences("Insights", Context.MODE_PRIVATE)
-
-    private enum class Preference {
-        JobId
-    }
+    private var SharedPreferences.jobId by SharedPreferencesDelegate.Int(defaultJobId)
 
     companion object {
 
@@ -26,8 +25,7 @@ internal class EventUploaderAndroidJob(context: Context) : EventUploader {
     }
 
     override fun startPeriodicUpload() {
-        val storedJobId = preferences.getInt(Preference.JobId.name, defaultJobId)
-        if (storedJobId == defaultJobId) {
+        if (preferences.jobId == defaultJobId) {
             val jobId = JobRequest
                 .Builder(EventJobCreator.Tag.Periodic.name)
                 .setRequiredNetworkType(JobRequest.NetworkType.CONNECTED)
@@ -36,7 +34,7 @@ internal class EventUploaderAndroidJob(context: Context) : EventUploader {
                     TimeUnit.MINUTES.toMillis(flexTimeIntervalInMinutes))
                 .build()
                 .schedule()
-            preferences.edit().putInt(Preference.JobId.name, jobId).apply()
+            preferences.jobId = jobId
         }
     }
 
