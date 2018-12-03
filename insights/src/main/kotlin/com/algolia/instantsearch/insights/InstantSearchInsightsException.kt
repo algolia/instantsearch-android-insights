92d9e7be873f9c667d4a1ddeb7e09c534fa0447f
Paul-Louis NECH <pln@algolia.com>
2018-12-03 13:04:53 +0100

    refactor(InsightsException): Improve DX with messages

--- insights/src/main/kotlin/com/algolia/instantsearch/insights/InstantSearchInsightsException.kt
+++ insights/src/main/kotlin/com/algolia/instantsearch/insights/InstantSearchInsightsException.kt
@@ -4,16 +4,16 @@ package com.algolia.instantsearch.insights
 /**
  * InstantSearch Insights exceptions.
  */
-sealed class InstantSearchInsightsException : Exception() {
+sealed class InstantSearchInsightsException(override val message: String? = null) : Exception(message) {
 
     /**
      * Will be thrown when you try to access an index through the [Insights.shared]
      * method without having registered the index through the [Insights.register] method first.
      */
-    class IndexNotRegistered : InstantSearchInsightsException()
+    class IndexNotRegistered : InstantSearchInsightsException("You need to call Insights.register before Insights.shared")
 
     /**
      * Will be thrown when you call [`insights.{search,personalization}.{view,click,conversion}()`][Insights.search] without [setting an userToken][Insights.userToken] first.
      */
-    class NoUserToken : InstantSearchInsightsException()
+    class NoUserToken : InstantSearchInsightsException("You need to set Insights.userToken first.")
 }
