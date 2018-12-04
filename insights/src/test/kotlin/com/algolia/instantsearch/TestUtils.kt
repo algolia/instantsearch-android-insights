43b23b812f3beb003c213408645a4bd8866b3e8e
Quentin LITZLER <qlitzler@gmail.com>
2018-12-04 14:24:38 +0100

    fix: make TestUtils parameters public

--- insights/src/test/kotlin/com/algolia/instantsearch/TestUtils.kt
+++ insights/src/test/kotlin/com/algolia/instantsearch/TestUtils.kt
@@ -9,20 +9,20 @@ import com.algolia.instantsearch.insights.webservice.WebServiceHttp
 
 internal object TestUtils {
 
-    private const val eventA = "EventA"
-    private const val eventB = "EventB"
-    private const val eventC = "EventC"
+    const val eventA = "EventA"
+    const val eventB = "EventB"
+    const val eventC = "EventC"
 
     const val indexName = "latency"
 
     private const val appId = BuildConfig.ALGOLIA_APPLICATION_ID
     private const val apiKey = BuildConfig.ALGOLIA_API_KEY
 
-    private const val queryId = "6de2f7eaa537fa93d8f8f05b927953b1"
-    private const val userToken = "foobarbaz"
-    private val positions = listOf(1)
-    private val objectIDs = listOf("54675051")
-    private val timestamp = System.currentTimeMillis()
+    const val queryId = "6de2f7eaa537fa93d8f8f05b927953b1"
+    const val userToken = "foobarbaz"
+    val positions = listOf(1)
+    val objectIDs = listOf("54675051")
+    val timestamp = System.currentTimeMillis()
 
     private val configuration = Insights.Configuration(
         connectTimeoutInMilliseconds = 5000,
