44cae8f84eb5c373aebff1091d85082bfc1b7bb0
Quentin LITZLER <qlitzler@gmail.com>
2019-06-17 12:55:45 +0200

    On demand WorkManager initialization

--- app/src/main/java/com/algolia/insights/App.java
+++ app/src/main/java/com/algolia/insights/App.java
@@ -1,13 +1,15 @@
 package com.algolia.insights;
 
 import android.app.Application;
-
+import androidx.annotation.NonNull;
+import androidx.work.Configuration;
+import androidx.work.WorkManager;
 import com.algolia.instantsearch.insights.Insights;
 import com.algolia.search.saas.Client;
 import com.algolia.search.saas.Index;
 import com.facebook.stetho.Stetho;
 
-public class App extends Application {
+public class App extends Application implements Configuration.Provider {
 
     static String appId = "latency";
     static String apiKey = "afc3dd66dd1293e2e2736a5a51b05c0a";
@@ -22,6 +24,7 @@ public class App extends Application {
         super.onCreate();
         Insights.Configuration configuration = new Insights.Configuration(5000, 5000);
 
+        WorkManager.initialize(this, getWorkManagerConfiguration());
         insights = Insights.register(this, appId, apiKey, indexName, configuration);
         insights.setLoggingEnabled(true);
         insights.setUserToken("userToken");
@@ -30,5 +33,11 @@ public class App extends Application {
         client = new Client(appId, apiKey);
         index = client.getIndex(indexName);
         Stetho.initializeWithDefaults(this);
+    }
+
+    @NonNull
+    @Override
+    public Configuration getWorkManagerConfiguration() {
+        return new Configuration.Builder().setMinimumLoggingLevel(android.util.Log.INFO).build();
     }
 }
